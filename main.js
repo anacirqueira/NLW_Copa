7a4cff2dff5be6ea81deb5407518c695866bf668
anacirqueira <cirqueiraana6@gmail.com>
2022-11-04 23:36:59 -0300

    Adicionando a função de createGames

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-function createCard() {
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+    </li>
+  `
+}
+
+function createCard(date, day, games) {
   return `
   <div class="card">
-  <h2>24/11 <span>Quinta</span></h2>
+  <h2>${date} <span>${day}</span></h2>
   <ul>
-    <li>
-      <img src="./assets/icon-brazil.svg" alt="Bandeira do Brasil">
-      <strong>16:00</strong>
-      <img src="./assets/icon-serbia.svg" alt="Bandeira da Serbia">
-    </li>
+    ${games}
   </ul>
 </div>  
   `
@@ -18,9 +24,12 @@ document.querySelector("#app").innerHTML = `
   <img src="./assets/logo.svg" alt="logo">
   </header>
   <main id="cards">
-  ${createCard()}
-  ${createCard()}
-  ${createCard()}
+  ${createCard("24/11", "quinta", 
+    createGame('brazil', '16:00', 'serbia'))}
+  ${createCard("28/11", "segunda", 
+    createGame('brazil', '16:00', 'serbia'))}
+  ${createCard("02/12", "sexta", 
+    createGame('brazil', '16:00', 'serbia'))}
   </main>
 `
 
