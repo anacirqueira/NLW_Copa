72b09e3feaafd5588f0c23aa6e2d74f2d72accd9
anacirqueira <cirqueiraana6@gmail.com>
2022-11-05 13:45:15 -0300

    refactor

--- main.js
+++ main.js
@@ -11,25 +11,20 @@ function createGame(player1, hour, player2) {
 function createCard(date, day, games) {
   return `
   <div class="card">
-  <h2>${date} <span>${day}</span></h2>
-  <ul>
-    ${games}
-  </ul>
+    <h2>${date} <span>${day}</span></h2>
+       <ul>
+        ${games}
+      </ul>
 </div>  
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-  <img src="./assets/logo.svg" alt="logo">
-  </header>
-  <main id="cards">
-  ${createCard("24/11", "quinta", 
-    createGame('brazil', '16:00', 'serbia'))}
-  ${createCard("28/11", "segunda", 
-    createGame('brazil', '13:00', 'switzerland'))}
-  ${createCard("02/12", "sexta", 
-    createGame('brazil', '16:00', 'cameroon'))}
-  </main>
-`
+document.querySelector("#cards").innerHTML = 
+
+  createCard("24/11", "quinta", 
+    createGame('brazil', '16:00', 'serbia')) +
+  createCard("28/11", "segunda", 
+    createGame('brazil', '13:00', 'switzerland')) +
+  createCard("02/12", "sexta", 
+    createGame('brazil', '16:00', 'cameroon'))
 
